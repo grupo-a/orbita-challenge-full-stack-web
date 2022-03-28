cfc23c1530821d9a177d87bc7d7db0cd908525ee
Lucas da Mata Silva <hefesttecnologia@gmail.com>
2022-03-27 23:20:26 -0300

    refactor: Refactor code to persist in the database

--- backend/backend/Data/Dtos/CreateStudentDTO.cs
+++ backend/backend/Data/Dtos/CreateStudentDTO.cs
@@ -1,23 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace backend.Models
+namespace backend.Data.Dtos
 {
-    public class Student
+    public class CreateStudentDTO
     {
-        [Key]
-        [Required]
-        public int Id { get; set; }
         [Required(ErrorMessage = "The name is required")]
         public string Name { get; set; }
         [Required(ErrorMessage = "The e-mail is required")]
         [EmailAddress(ErrorMessage = "Invalid e-mail")]
-        public string Email { get; set; } 
+        public string Email { get; set; }
         [Required(ErrorMessage = "The RA is required")]
         public string Ra { get; set; }
         [Required(ErrorMessage = "The CPF is required")]
         [RegularExpression("^([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11})|([0-9]{9}-[0-9]{2})$", ErrorMessage = "CPF must have 11 digits")]
         public string Cpf { get; set; }
-
-
     }
 }
