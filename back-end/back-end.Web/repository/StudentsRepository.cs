1dd65e0e2db1cd4c8928a37e17a83b8d205486c0
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-06 20:22:17 -0300

    feat: StudentsRepository

---
+++ back-end/back-end.Web/repository/StudentsRepository.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using students_db.Models;
+
+namespace students_db.Repository;
+
+public class StudentsRepository
+{
+    protected readonly StudentsContext _context;
+    public StudentsRepository(StudentsContext context)
+    {
+        _context = context;
+    }
+
+    public List<Student?> GetAll()
+    {
+        return _context.Students.ToList();
+    }
+
+    public Student GetById(int ra)
+    {
+        return _context.Students.Where(e => e.RA == ra).First();
+    }
+
+    public Student Insert(Student student)
+    {
+        _context.Add(student);
+        _context.SaveChanges();
+        return student;
+    }
+
+    public virtual void Update(Student student)
+    {
+        _context.Update(student);
+        _context.SaveChanges();
+    }
+
+    public virtual void Delete(int ra)
+    {
+        var student = _context.Students.Where(e => e.RA == ra).First();
+        _context.Remove(student);
+        _context.SaveChanges();
+    }
+}
