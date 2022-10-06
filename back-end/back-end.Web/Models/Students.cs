12324c86e6b896eeba1e46e0440ec2f541c18d6e
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-06 17:19:28 -0300

    feat: model Students

---
+++ back-end/back-end.Web/Models/Students.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace students_db.Models;
+public class Students
+{
+    [Key]
+    [Required(ErrorMessage="Required field")]
+    public int RA { get; set; }
+    [Required(ErrorMessage="Required field")]
+    [MinLength(3, ErrorMessage="Name must be at least 3 characters long")]
+    public string Name { get; set; }
+    [Required(ErrorMessage="Required field")]
+    [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Email must be in a valid format")]
+    public string Email { get; set; }
+    [Required(ErrorMessage="Required field")]
+    [Range(11, int.MinValue, ErrorMessage = "CPF must be in a valid format")]
+    public int CPF { get; set; }
+
+}
