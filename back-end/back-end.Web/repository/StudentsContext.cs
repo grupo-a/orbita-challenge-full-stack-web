4196bf767b5135e1ed308f973dd6df816fcae3e2
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-06 18:10:51 -0300

    feat: add StidentsContext

---
+++ back-end/back-end.Web/repository/StudentsContext.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using students_db.Models;
+
+namespace students_db.Repository;
+
+public class StudentsContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+
+    public StudentsContext(DbContextOptions<StudentsContext> options)
+    : base(options)
+    {}
+    // public void ConfigureServices(IServiceCollection services)
+    // {
+    //     var connection = Environment.GetEnvironmentVariable("ConexaoMySql:MySqlConnectionString");
+
+    //     services.AddDbContext<StudentsContext>(options =>
+    //         options.UseMySql(connection)
+    //     );
+
+    //     services.AddMvc();
+    // }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("DOTNET_CONNECTION_STRING");
+
+            optionsBuilder.UseMySql(
+                connectionString,
+                new MySqlServerVersion(new Version(8, 0, 11))
+            );
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>()
+        .HasKey(s => s.RA);
+    }
+}
